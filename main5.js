14ebf95e2e6cfbcbc74072a4dc279ffdfed3c604
Red_Robles <redworld22@hotmail.com>
2024-07-21 15:42:31 -0400

    Clase y ejercicio de Module Pattern y NameSpaces: propiedades privadas en JavaScript

--- main5.js
+++ main5.js
@@ -12,8 +12,10 @@ function createStudent({
     approvedCourses = [],
     learningPaths = []
 } = {}){
-    return {
-        name,
+    const private = {
+        "_name": name
+    };
+    const public = {
         email,
         age,
         socialMedia: {
@@ -22,8 +24,26 @@ function createStudent({
             facebook
         },
         approvedCourses,
-        learningPaths
+        learningPaths,
+        readName(){
+            return private["_name"];
+        },
+        changeName(newName){
+            private["_name"] = newName;
+        }
     }
+
+    Object.defineProperty(public, "readName", {
+        writable: false,
+        configurable: false
+    })
+
+    Object.defineProperty(public, "changeName", {
+        writable: false,
+        configurable: false
+    })
+    
+    return public;
 }
 
 const juan = createStudent({
